3cf4fbff528b524849123822a0f540f7923fd607
Sourabh Gupta <sourabhgupta.1331@gmail.com>
2026-02-05 22:49:05 +0530

    create connect wallet page

---
+++ frontend/src/components/Connect.js
@@ -0,0 +1,33 @@
+import React from 'react'
+import { useNavigate } from 'react-router-dom';
+
+const Connect = () => {
+    const navigateTo=useNavigate()
+    const connectWallet=async(address)=>{
+    try {
+        if(window.ethereum){
+            const account=await window.ethereum.request({method:'eth_requestAccounts'});
+            navigateTo("/home",{state:{address:account[0]}})
+        }
+        else{
+            alert("Install Metamask")
+        }
+    } catch (error) {
+        console.log(error)
+    }
+}
+
+  return (
+    <div className="card">
+  <h1>Connect Wallet</h1>
+  <p>Please connect your MetaMask wallet to continue.</p>
+
+  <button className="btn" onClick={connectWallet}>
+    Connect Wallet
+  </button>
+</div>
+
+  )
+}
+
+export default Connect
