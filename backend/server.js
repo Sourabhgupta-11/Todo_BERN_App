5abd8d4dbe1643b86b9b161496fac098e9805c19
Sourabh Gupta <sourabhgupta.1331@gmail.com>
2026-02-06 05:55:38 +0530

    made backend ready for deployment

--- backend/server.js
+++ backend/server.js
@@ -2,7 +2,7 @@ require("dotenv/config")
 const express=require("express");
 const cors=require( "cors")
 const ethers=require("ethers")
-const Abi=require( "../contract/artifacts/contracts/todo.sol/TodoApp.json" )
+const Abi=require( "../backend/TodoApp.json" )
 
 const app=express();
 app.use(express.json());
